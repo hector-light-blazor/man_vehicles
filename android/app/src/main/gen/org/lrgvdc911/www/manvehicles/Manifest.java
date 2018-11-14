1d6e74526dfe1d8306ae343972ba54cfbe71c1f2
noe <hchapa@lrgvdc911.org>
2018-11-14 10:08:01 -0600

    New file for home page.

---
+++ android/app/src/main/gen/org/lrgvdc911/www/manvehicles/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package org.lrgvdc911.www.manvehicles;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
